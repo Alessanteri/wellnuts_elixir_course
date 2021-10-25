d3698bef37cfe5020f67f1f6321a70e57b2ccf08
Alessanteri <alessanterio@mail.ru>
2021-10-25 18:30:31 +0300

    Fix bags

--- number_of_days_between_two_dates/main.ex
+++ number_of_days_between_two_dates/main.ex
@@ -1,14 +1,9 @@
 defmodule Solution do
   @spec days_between_dates(date1 :: String.t(), date2 :: String.t()) :: integer
   def days_between_dates(date1, date2) do
-    Solution.sing(Date.diff(Date.from_iso8601!(date1), Date.from_iso8601!(date2)))
+    sing(Date.diff(Date.from_iso8601!(date1), Date.from_iso8601!(date2)))
   end
 
-  def sing(result) when result < 0, do: result * -1
-  def sing(result) when result >= 0, do: result
+  defp sing(result) when result < 0, do: abs(result)
+  defp sing(result) when result >= 0, do: result
 end
-
-date1 = "2019-06-29"
-date2 = "2019-06-30"
-
-IO.puts(Solution.days_between_dates(date1, date2))
