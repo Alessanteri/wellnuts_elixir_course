cfc90764804633c818955a4d0c17c8db79c043ec
Alessanteri <alessanterio@mail.ru>
2021-10-25 18:38:32 +0300

    Replace function sing to abs

--- number_of_days_between_two_dates/main.ex
+++ number_of_days_between_two_dates/main.ex
@@ -1,9 +1,6 @@
 defmodule Solution do
   @spec days_between_dates(date1 :: String.t(), date2 :: String.t()) :: integer
   def days_between_dates(date1, date2) do
-    sing(Date.diff(Date.from_iso8601!(date1), Date.from_iso8601!(date2)))
+    abs(Date.diff(Date.from_iso8601!(date1), Date.from_iso8601!(date2)))
   end
-
-  defp sing(result) when result < 0, do: abs(result)
-  defp sing(result) when result >= 0, do: result
 end
